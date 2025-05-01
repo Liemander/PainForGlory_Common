de46e46b2d01626cda0ba307c9e38232dc6c762b
Justin Gammell <Liemander@outlook.com>
2025-05-01 05:51:50 -0500

    Basic Admin UI done

--- obj/Debug/net8.0/PainForGlory_Common.AssemblyInfo.cs
+++ obj/Debug/net8.0/PainForGlory_Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PainForGlory_Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e9f13bbad02603139dd27dc94c3569a8fb9cc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61753b155d739fdf59ef13a706fd9566eb2b3e6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PainForGlory_Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PainForGlory_Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
