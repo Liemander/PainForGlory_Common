1b36fc003d3f522eff5262558a859f3b665a51c9
Justin Gammell <Liemander@outlook.com>
2025-05-09 09:45:02 -0500

    Renamed Shard server and added Zone Server

--- obj/Release/net8.0/PainForGlory_Common.AssemblyInfo.cs
+++ obj/Release/net8.0/PainForGlory_Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PainForGlory_Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b686e40ffc46b197226da0fea448e5bb59ffc64e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016f5432804f428ecaa8ffe0486f3f07aac6d267")]
 [assembly: System.Reflection.AssemblyProductAttribute("PainForGlory_Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PainForGlory_Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
